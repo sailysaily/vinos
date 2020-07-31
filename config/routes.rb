a03c20edd8dd1a49a5ba07e1aee57e313c73577e
(Simon Martinez) <smartineze1993@gmail.com>
2020-07-30 22:46:05 -0500

    Added all my routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "wines#home"
-  get "/wines", to: "wines#index"
+  root to: 'wines#home'
+  # #Real all wines
+  # get '/wines', to: 'wines#index'
+  # #read one wine
+  # get '/wines/:id', to: 'wines#show'
+  # # create a wine
+  # get '/wines/new', to: 'wines#new'
+  # post '/wines', to: 'wines#create'
+  # # update a wine
+  # get '/wines/:id/edit', to: 'wines#edit'
+  # patch '/wines/:id', to: 'wines#update'
+  # #delete a wine
+  # delete 'wines/:id', to: 'wines#destroy'
+
+  resources :wines
 end
