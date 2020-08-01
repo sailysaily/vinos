0cd833e72434c7f7dab6474e57c5036ea7d386b9
(Simon Martinez) <smartineze1993@gmail.com>
2020-07-31 20:58:35 -0500

    Added env and some gems

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,7 +1,26 @@
 class WinesController < ApplicationController
-  def index
+  def home
   end
 
-  def home
+  def index
+    @wines = Wine.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
