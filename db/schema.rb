97ca3a06c47a74151dd14a20886e2d85f8cb47a9
(Simon Martinez) <smartineze1993@gmail.com>
2020-08-01 19:53:40 -0500

    Added price to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_023513) do
+ActiveRecord::Schema.define(version: 2020_08_01_154934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_023513) do
     t.integer "year"
     t.text "story"
     t.string "background_image"
+    t.float "price"
   end
 
 end
